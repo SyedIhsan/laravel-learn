4f99d753f0d129fa0de45efc7d980564dda16bc5
SyedIhsan <syedihsan1998@gmail.com>
2026-04-25 00:41:05 +0800

    let's do it

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/', [ChirpController::class, 'index']);
+
+Route::middleware('auth')->group(function() {
+    Route::post('/chirps', [ChirpController::class, 'store']);
+    Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+    Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+    Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
 });
 
-Route::get('/register', function () {
-    return view('register');
-});
+// Registration routes
+Route::view('/register', 'auth.register')->name('register')->middleware('guest');
+Route::post('/register', Register::class)->middleware('guest');
+
+// Logout routes
+Route::post('/logout', Logout::class)->middleware('auth')->name('logout');
+
+// Login routes
+Route::view('/login', 'auth.login')->name('login')->middleware('guest');
+Route::post('/login', Login::class)->middleware('guest');
